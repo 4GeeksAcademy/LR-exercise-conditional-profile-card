53f08a61d423f6011af506fc0412cca87ecf46e8
luisrico13 <luisrico13@proton.me>
2024-12-22 21:13:38 +0000

    trabajo listo

--- src/js/app.js
+++ src/js/app.js
@@ -1,49 +1,58 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should show the cover image
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the image's url that will be used as a background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "right", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastName: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
+// This function is called every time the user changes types or changes any input
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); // print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
-  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
-  if (variables.includeCover == false) cover = "<div class='cover'></div>";
+  console.log("These are the current variables: ", variables); // Print the current variables to the console
 
-  // reset the website body with the new html output
-  document.querySelector("#widget_content").innerHTML = `<div class="widget">
-            ${cover}
-          <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-        </div>
-    `;
+  // Handle the cover image logic
+  let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
+  if (variables.includeCover === false) cover = "<div class='cover'></div>";
+
+  // Handle dynamic name, role, city, and country
+  let fullName = `${variables.name || "First Name"} ${variables.lastName ||
+    "Last Name"}`;
+  let role = variables.role || "Web Developer";
+  let location = `${variables.city || "City"}, ${variables.country ||
+    "Country"}`;
+
+  // Handle social media links dynamically
+  const socialMediaLinks = {
+    twitter: variables.twitter
+      ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+      : "",
+    github: variables.github
+      ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+      : "",
+    linkedin: variables.linkedin
+      ? `<li><a href="https://linkedin.com/in/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+      : "",
+    instagram: variables.instagram
+      ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+      : ""
+  };
+
+  // Join the social media links and set the position
+  const socialMediaBar = `
+    <ul class="position-${variables.socialMediaPosition || "left"}">
+      ${socialMediaLinks.twitter}
+      ${socialMediaLinks.github}
+      ${socialMediaLinks.linkedin}
+      ${socialMediaLinks.instagram}
+    </ul>
+  `;
+
+  // Reset the website body with the new HTML output
+  document.querySelector("#widget_content").innerHTML = `
+    <div class="widget">
+      ${cover}
+      <img src="${variables.avatarURL ||
+        "https://via.placeholder.com/150"}" class="photo" />
+      <h1>${fullName}</h1>
+      <h2>${role}</h2>
+      <h3>${location}</h3>
+      ${socialMediaBar}
+    </div>
+  `;
 }
 
 /**
@@ -51,15 +60,10 @@ function render(variables = {}) {
  */
 window.onload = function() {
   window.variables = {
-    // if includeCover is true the algorithm should show the cover image
     includeCover: true,
-    // this is the image's url that will be used as a background for the profile cover
     background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da",
-    // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
-    // social media bar position (left or right)
-    socialMediaPosition: "position-left",
-    // social media usernames
+    socialMediaPosition: "left",
     twitter: null,
     github: null,
     linkedin: null,
@@ -70,12 +74,13 @@ window.onload = function() {
     country: null,
     city: null
   };
-  render(window.variables); // render the card for the first time
+
+  render(window.variables); // Render the card for the first time
 
   document.querySelectorAll(".picker").forEach(function(elm) {
     elm.addEventListener("change", function(e) {
-      // <- add a listener to every input
-      const attribute = e.target.getAttribute("for"); // when any input changes, collect the value
+      // Add a listener to every input
+      const attribute = e.target.getAttribute("for"); // When any input changes, collect the value
       let values = {};
       values[attribute] =
         this.value == "" || this.value == "null"
@@ -85,7 +90,7 @@ window.onload = function() {
           : this.value == "false"
           ? false
           : this.value;
-      render(Object.assign(window.variables, values)); // render again the card with new values
+      render(Object.assign(window.variables, values)); // Render again the card with new values
     });
   });
 };
